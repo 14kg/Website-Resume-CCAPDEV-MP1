a7bce1b27fa3250662bade444bb89f6120075e06
14kg <67892939+14kg@users.noreply.github.com>
2020-08-13 15:59:22 +0800

    Firebase authentication functionality

--- script/term.js
+++ script/term.js
@@ -1,27 +1,44 @@
 var db = firebase.firestore();
 
-$('body').terminal({
+let t = $('body').terminal({
     hello: function(what) {
         this.echo('Hello, ' + what +
                   '. Welcome to this terminal.');
+        this.insert('hello', false);
     },
 
-    login: function(user, pass) {
-        let t = this;
-        firebase.auth().signInWithEmailAndPassword(user, pass).then(function(){
-            console.log("user is signed in yo");
-            t.set_prompt(user+'>');
-        }).catch(function(err){
-            if(err.code == "auth/wrong-password"){
-                console.log("wrong password");
-            }else{
-                console.log(err.message);
+    login: function() {
+        let u = 'email';
+        let p = 'password';
+        this.push(function(pass) {
+            if (pass) {
+                this.pop();
+                p = pass;
+                firebase.auth().signInWithEmailAndPassword(u, p).then(function(){
+                    console.log("user is signed in yo");
+                    t.set_prompt('[[;#087830;]'+u+']>');
+                    t.clear();
+                    t.echo("Logged in");
+                }).catch(function(err){
+                    t.echo(err.message);
+                    console.log(err.message);
+                })
             }
-        })
+        }, {
+            prompt: 'password: '
+        });
+        this.push(function(user) {
+            if (user) {
+                this.pop();
+                this.set_mask('*');
+                u = user;
+            }
+        }, {
+            prompt: 'email: '
+        });
     },
     
     education: function() {
-        let t = this;
         db.collection("education").get().then(function(snapshot){
             snapshot.forEach(function(doc){
                 d = doc.data();
@@ -29,9 +46,18 @@ $('body').terminal({
                 t.echo(d.degree);
                 t.echo(d.school);
                 t.echo(d.year_start+"-"+d.year_end);
+                t.echo('<a href="index.html">hello</a>', {raw: true});
             })
         });    
     }
 }, { 
-    greetings: 'hello world :-)'
+    greetings: 
+    '888               888 888          \n'+
+    '888               888 888          \n'+
+    '888               888 888          \n'+
+    '88888b.   .d88b.  888 888  .d88b.  \n'+
+    '888 "88b d8P  Y8b 888 888 d88""88b \n'+
+    '888  888 88888888 888 888 888  888 \n'+
+    '888  888 Y8b.     888 888 Y88..88P \n'+
+    '888  888  "Y8888  888 888  "Y88P"  \n'
 });
